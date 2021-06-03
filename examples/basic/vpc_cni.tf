0105d8a2d477501e07f05197d2469a5fb6a26ae4
Arthur Diniz <arthurbdiniz@gmail.com>
2021-06-03 00:50:45 -0300

    Format example eks module name
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- examples/basic/vpc_cni.tf
+++ examples/basic/vpc_cni.tf
@@ -2,10 +2,9 @@ module "vpc_cni" {
   source  = "DNXLabs/eks-vpc-cni/aws"
   version = "0.1.0"
 
-  cluster_identity_oidc_issuer     = module.eks_cluster.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks_cluster.oidc_provider_arn
-  cluster_name                     = module.eks_cluster.cluster_id
-  region                           = data.aws_region.current.name
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_name                     = module.eks.cluster_id
 
   depends_on = [module.eks]
 }
